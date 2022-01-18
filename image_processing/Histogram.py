0152438a4f9cc353319e2667bcb50d3a21ab082c
Krzysztof Dziurlikowski <krzysztof.dziurlikowski@atendesoftware.pl>
2022-01-18 21:48:20 +0100

    Add script counting histogram and scale images in dataset.
    HSV "value" is histogramed and scaled

---
+++ image_processing/Histogram.py
@@ -0,0 +1,60 @@
+import os
+from glob import glob
+from os import listdir
+from os.path import isfile, join
+
+import numpy as np
+import cv2
+
+
+class Histogram:
+    def __init__(self, dirs, lower_threshold=0.05, upper_threshold=0.95):
+        self.dirs = dirs
+        self.lower = 0
+        self.upper = 255
+
+        self.cumulative_lower_threshold = lower_threshold
+        self.cumulative_upper_threshold = upper_threshold
+        self.histogram = {}
+        self.pix_cnt = 0
+        self.__process_pixel_vectorized = np.vectorize(self.__process_pixel)
+
+    def count_histogram(self):
+        for dyr in self.dirs:
+            labels_dirs = glob(os.path.join(dyr, '*'))
+
+            for label_dir in labels_dirs:
+                image_files = [os.path.join(label_dir, f) for f in listdir(label_dir) if isfile(join(label_dir, f))]
+                for file in image_files:
+                    image = cv2.imread(file)
+                    hsvim = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+                    self.__process_image(hsvim)
+        self.__count_lower_upper()
+
+    def save_results(self):
+        with open('stretching-results', 'w') as f:
+            f.write('lower: {}'.format(self.lower))
+            f.write('upper: {}'.format(self.upper))
+
+    def __process_image(self, hsvImage):
+        v_channel = hsvImage[:, :, 2]
+        self.__process_pixel_vectorized(v_channel)
+
+    def __process_pixel(self, value: int):
+        if value not in self.histogram:
+            self.histogram[value] = 0
+        self.histogram[value] += 1
+        self.pix_cnt += 1
+        return value
+
+    def __count_lower_upper(self):
+        cumulative = 0.0
+        for pix_val, count in sorted(self.histogram.items(), key=lambda item: item[0]):
+            delta = float(count) / self.pix_cnt
+            cumulative += delta
+            if cumulative <= self.cumulative_lower_threshold:
+                self.lower = pix_val
+            if cumulative <= self.cumulative_upper_threshold:
+                self.upper = pix_val
+
+            assert (cumulative <= 1.0)
--- scripts/stretch_histogram.py
+++ scripts/stretch_histogram.py
@@ -1,43 +1,11 @@
-import os
-from glob import glob
-from os import listdir
-from os.path import isfile, join
-
-import numpy as np
-from PIL import Image
-from conf.Config import Config
-
-
-class Scaler:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-        self.scale_pixel_vect = np.vectorize(self.scale_pixel)
-
-    def scale_pixel(self, pix: int):
-        if pix < self.a:
-            return 0
-        if pix > self.b:
-            return 255
-        return int(255 * (pix - self.a) / (self.b - self.a))
-
-    def scale(self, image: Image) -> Image:
-        image_as_array = np.array(image)
-        scaled = self.scale_pixel_vect(image_as_array)
-        return Image.fromarray(np.uint8(scaled))
-
+from image_processing.Histogram import Histogram
+from image_processing.PixelScaler import PixelScaler
 
 dirs = ['../dataset/single-coins/train', '../dataset/single-coins/validation', '../dataset/single-coins/test']
 
-scaler = Scaler(50, 200)
+histogram = Histogram(dirs, lower_threshold=0.05, upper_threshold=0.95)
+histogram.count_histogram()
+histogram.save_results()
 
-for dyr in dirs:
-    labels_dirs = glob(os.path.join(dyr, '*'))
-    labels_cnt = len(labels_dirs)
-
-    for label_dir in labels_dirs:
-        image_files = [os.path.join(label_dir, f) for f in listdir(label_dir) if isfile(join(label_dir, f))]
-        for file in image_files:
-            image = Image.open(file)
-            scaled = scaler.scale(image)
-            scaled.save(file)
+scaler = PixelScaler(dirs, histogram.lower, histogram.upper)
+scaler.scale_images_in_dirs()
